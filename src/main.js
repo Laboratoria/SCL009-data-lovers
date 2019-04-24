874d406681c83a8b7865d0b1924adb928bf16de3
rositadiazgalaz <rositadiazgalaz@gmail.com>
2019-04-24 12:04:11 -0400

    mostrando imagen

--- src/main.js
+++ src/main.js
@@ -16,11 +16,30 @@ selectChange.addEventListener('change', () =>{
     }else{
     return data[element].tags.includes(selectedTag)}
     })
-    console.log(resultFilterTag)
+
+    console.log(resultFilterTag);
+
     championImg.innerHTML = "";
+    //resultFilterTag.forEach(element=>{})
     for(let i=0;i<resultFilterTag.length;i++){
-      championImg.innerHTML += `<div> ${resultFilterTag[i]} </div>`}
-    
+      //championImg.innerHTML += `<div class="estilo"> ${resultFilterTag[i]} </div>`}
+      championImg.innerHTML += `<div class="card" id="cards" style="width: 18rem;">
+  <img src="${element.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${resultFilterTag[i]}</h5>
+    <p class="card-text">${element.blurb}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`
+  }
 
 })
         
