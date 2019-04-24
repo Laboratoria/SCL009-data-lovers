fd373bb385275f7ca77409aa7ae73109c8490f94
Cloiw <sircli@hotmail.com>
2019-04-24 09:26:23 -0400

    funcion filtrar (probando)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 /* Manejo del DOM */
+const data= LOL.data;
+const dataKeys= Object.keys(data)
+const championImg = document.getElementById("championImg");
+const selectChange = document.getElementById("userSelectedTag");
+
+
+selectChange.addEventListener('change', () =>{ 
+     
+  let selectedTag = document.getElementById("userSelectedTag").value
+  
+   
+  const resultFilterTag = dataKeys.filter( (element)=>{
+    return data[element].tags.includes(selectedTag)})
+
+
+   }
+   
+})
+   
+        
+      
+   
+;
+
+
+  
