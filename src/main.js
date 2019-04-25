0a12df677fe046a990421935fa8cd63a75fde2ce
Cloiw <sircli@hotmail.com>
2019-04-25 12:53:21 -0400

    separando js

--- src/main.js
+++ src/main.js
@@ -1,37 +1,44 @@
 /* Manejo del DOM */
-const data= LOL.data;
-const dataKeys= Object.keys(data)
+const data= LOL.data; // ingresar a la data
+const dataKeys= Object.keys(data) // cada uno de los champs
 const championImg = document.getElementById("championImg");
 const selectChange = document.getElementById("userSelectedTag");
 
 
 selectChange.addEventListener('change', () =>{ 
-     
-  let selectedTag = document.getElementById("userSelectedTag").value
+   
+  let selectedTag = document.getElementById("userSelectedTag").value;
+  
+  
  
-   
-  const resultFilterTag = dataKeys.filter( (element)=>{
-    if(selectedTag==="Todos"){
-      return data[element];
-    }else{
-    return data[element].tags.includes(selectedTag);}
-   
-    })
-    
-    
-    
-    
-    championImg.innerHTML = "";
-    for(let i=0;i<resultFilterTag.length;i++){
-      let champName = resultFilterTag[i]
-      let champImg = data[champName].img
-      championImg.innerHTML += `<div class="estilo"> ${ resultFilterTag[i]} <img src="${champImg}">  </div>`}
-    
+ championImg.innerHTML = ""; //
+ for(let i=0;i<filteringResult(selectedTag,data).length;i++){
+  champName= (filteringResult(selectedTag,data)[i]);
 
-})
+  championImg.innerHTML += `<div class="card" style="width: 12rem;">
+  <img id="tamaño" src="" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p id="textNameCard" class="card-text">${champName}</p>
+  </div>
+</div>`
+
+ 
+ }
+});
+
+
+
+
+
+
+ 
+    
+    
+    
+    
+    
         
       
    
 
 
-  
