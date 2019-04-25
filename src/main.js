4e42ec53d882d83500ce814842a96bc55f35c521
betanyeli <betangelii@gmail.com>
2019-04-24 22:45:12 -0400

    Variable global lol.js y main.js, filtrar por rol en consola

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-/* Manejo del DOM */
+const accessValue = Object.values(LOL.data); // OB arr q contiene los valores de las prop de obj
+console.log(LOL.data);
+accessValue.forEach(elemento => {
+    console.log(elemento);
+});  // asi me da ahora. 
+
+
+const btnAssasin = document.getElementById("assassin");
+btnAssasin.addEventListener("click", () => {
+    const championTags = "Assasin"; // declaro una constante con el nombre exacto que aparece en mi lol.js
+    const filterRol = accessValue.filter((element) => { // le hago el filter a mi funcion global
+        return (element.tags[0] === "Assassin" || element.tags[1] === "Assassin");
+      });
+      return console.log(filterRol);
+})
