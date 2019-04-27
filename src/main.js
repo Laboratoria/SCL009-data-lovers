630a2da0a15d519dac8b078b437d5fee07d16c80
betanyeli <betangelii@gmail.com>
2019-04-27 18:47:13 -0400

    cambios html en option value, y cambios main js quitar value

--- src/main.js
+++ src/main.js
@@ -10,27 +10,6 @@ accessValue.forEach(element => {
 function tagsSelect() {
     const selecRol= document.getElementById("rol-lol");
     const optionSelect = selecRol.options[selecRol.selectedIndex].value;   
-const resultado = window.filterData(accessValue, optionSelect.value);
+const resultado = window.filterData(accessValue, optionSelect);
 console.log(resultado); 
 }
-
-
-
-
-
-// lo de abajo es con btns 27 abril
-/*const accessValue = Object.values(LOL.data); // OB arr q contiene los valores de las prop de obj
-console.log(LOL.data);
-accessValue.forEach(elemento => {
-    console.log(elemento);
-});  // asi me da ahora. 
-
-
-const btnAssasin = document.getElementById("assassin");
-btnAssasin.addEventListener("click", () => {
-    const championTags = "Assasin"; // declaro una constante con el nombre exacto que aparece en mi lol.js
-    const filterRol = accessValue.filter((element) => { // le hago el filter a mi funcion global
-        return (element.tags[0] === "Assassin" || element.tags[1] === "Assassin");
-      });
-      return console.log(filterRol);
-}) */
