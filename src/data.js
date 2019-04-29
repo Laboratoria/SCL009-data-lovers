84f9b08bf6125ebc8c9eebc15c5179e3d91cf30f
betanyeli <betangelii@gmail.com>
2019-04-29 16:28:44 -0400

    eliminar funcion onchange, mostrar data en html dinamico, enlaces rotos en lol.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,27 @@
-const filterData = (data, rol) => {
+/*const filterData = (data, rol) => {
   const nowFilter = data.filter ((element) => {
       return (element.tags[0] === rol || element.tags[1] === rol );
   });
  return nowFilter;
 };
-window.filterData =  filterData;
+window.filterData =  filterData; */
+
+const filterData = (data, rol) => {
+  const nowFilter = data.filter ((element) => {
+    return element.tags.includes(rol);
+  });
+  return nowFilter;
+};
+window.filterData =  filterData; 
+
+const sortData = (data, sortBy, sortOrder) => {
+  const nowSort = data.sort ((low, high)=> {
+    if (low.name > high.name) {
+      return 1;
+    } if (low.name < high.name) {
+      return 0;
+    }
+  });
+  return sortData;
+};
+window.sortData = sortData;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,42 @@
 const accessValue = Object.values(LOL.data); // OB arr q contiene los valores de las prop de obj
-    //console.log(accessValue);
+const containerRol = document.getElementById("container-rol"); // div que imprime lo q seleccione en select
+const container = document.getElementById("container-result"); //div q contiene todos mis campeones
+
+const btnFilter = document.getElementById("select-filter");
+btnFilter.addEventListener("click", () => {
+    containerRol.innerHTML = "";
+    const selectRol = document.getElementById("rol-lol").value
+    const result = window.filterData(accessValue, selectRol);
+    const showChampionsRol = "";
+    const showRol = result.forEach(element => {
     
-accessValue.forEach(element => {
-    element.tags;
-});  // asi me da ahora.  
+        containerRol.innerHTML += `  <div class="card col-4">
+        <img class="card-img-top" src="${element.img}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${element.name}</h5>
+          <p class="card-text">${element.title} </p>
+          <p class="card-text">${element.tags} </p>
+          <p class="card-text">${element.blurb} </p>
+          <p class="card-text"><small class="text-muted">WinnerLegends</small></p>
+        </div>
+      </div>
+   
+   `
+    })
+})
 
-////////////////////////////////////////////////////////////////////////////
 
-function tagsSelect() {
-    const selecRol= document.getElementById("rol-lol");
-    const optionSelect = selecRol.options[selecRol.selectedIndex].value;   
-const resultado = window.filterData(accessValue, optionSelect);
-console.log(resultado); 
-}
+
+
+const btnChampions = document.getElementById("champions");
+btnChampions.addEventListener("click", () => {
+    const showChampions = "";
+    const showNow = accessValue.forEach(element => {
+        container.innerHTML += ` <div class="card col-4" style="width: 18em;">
+<img class="card-img-top" src="${element.splash}" alt="Card image cap">
+<div class="card-body"> ${element.name} <br> ${element.title} 
+<div class="text-info"> ${element.tags} <br> ${element.blurb}  </div>
+</div>
+`
+    });
+})
