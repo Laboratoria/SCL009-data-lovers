14c0c68acb91de30e8d7b288b65f44db52ad3f6d
nohestmm <nohestmm@gmail.com>
2019-04-29 11:33:39 -0300

    actualizando html

--- src/data.js
+++ src/data.js
@@ -3,18 +3,6 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
-
-for (let i=0;i<POKEMON.pokemon.length;i++){
-console.log(POKEMON.pokemon[i].name);
-console.log(POKEMON.pokemon[i].id);
-
-
-}
- 
 
 
 
-window.example = example;
