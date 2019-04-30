73acb0be97a88e9274e75f51dafaceb5dabf9d20
nohestmm <nohestmm@gmail.com>
2019-04-30 10:37:46 -0300

    actualizando ordenar y filtrar

--- src/data.js
+++ src/data.js
@@ -6,63 +6,62 @@
 
 const filterData =(data, condition) =>{
 
-    const datatype = data.filter(elemento => {
-    
-    return elemento.type.includes(condition);
+const datatype = data.filter(elemento => {
+
+return elemento.type.includes(condition);
+});
+
+return datatype;
+};
+ 
+
+
+
+const sortData = (data, sortBy, sortOrder) =>{
+
+let dataSort;
+
+    if (sortOrder === "ascaz"){
+    dataSort = data.sort((a,b) => {
+        return a[sortBy].localeCompare(b[sortBy]);
     });
+
+}
+
+else if (sortOrder === "descza"){
+    dataSort = data.sort((a,b) => {
+         return b[sortBy].localeCompare(a[sortBy]);
+     });
+ 
+ }
+
+ else if (sortOrder === "ascnum"){
+    dataSort = data.sort((a,b) => {
+         return a[sortBy].localeCompare(b[sortBy]);
+     });
+ 
+ }
+ 
+ else if (sortOrder === "descnum"){
+    dataSort = data.sort((a,b) => {
+         return b[sortBy].localeCompare(a[sortBy]);
+     });
+ 
+ }
     
-    return datatype;
-    };
-     
-    
-    
-    
-    const sortData = (data, sortBy, sortOrder) =>{
-    
-    let dataSort;
-    
-        if (sortOrder === "ascaz"){
-        dataSort = data.sort((a,b) => {
-            return a[sortBy].localeCompare(b[sortBy]);
-        });
-    
-    }
-    
-    else if (sortOrder === "descza"){
-        dataSort = data.sort((a,b) => {
-             return b[sortBy].localeCompare(a[sortBy]);
-         });
-     
-     }
-    
-     else if (sortOrder === "ascnum"){
-        dataSort = data.sort((a,b) => {
-             return a[sortBy].localeCompare(b[sortBy]);
-         });
-     
-     }
-     
-     else if (sortOrder === "descnum"){
-        dataSort = data.sort((a,b) => {
-             return b[sortBy].localeCompare(a[sortBy]);
-         });
-     
-     }
-        
-     return dataSort;
-    
-    }
-    
-    
-    
-    
-    window.filterData = filterData;
-    
-    window.sortData = sortData;
-    
-    
-    
-    
-    
-       
-    
+ return dataSort;
+
+}
+
+
+
+
+window.filterData = filterData;
+
+window.sortData = sortData;
+
+
+
+
+
+   
