2c565ad058fbb1bf1ec6bc430b95cebab1ab1c20
DayMateluna <d.mateluna.c@gmail.com>
2019-05-02 12:57:38 -0400

    test filtrar y ordenar primero

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,25 +3,97 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+  const data = [
+    {"num": "010",
+    "name": "Caterpie",
+    "type": [
+      "Bug"]
+    },
+    
+    {"num": "001",
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"]
+    },    
+    
+      {"num": "004",
+    "name": "Charmander",
+    "type": [
+      "Fire"]
+    } 
+]
+
 
 describe('filterData', () => {
-  // declara data yb pasarla al test para corroborar que filtra;
+  
+
+
+
+
+
+
   it('debería ser una función', () => {
     assert.equal(typeof filterData, 'function');
   });
 
-  it('debería retornar "Pikachu para cuando filtro por tipo electrico"', () => {
-    assert.equal(typeof  filterData, 'function');
+  it('debería retornar "Charmander para cuando filtro por tipo Fire"', () => {
+    assert.deepEqual(window.filterData(data,'Fire'), [{
+      "num": "004","name": "Charmander", "type": ["Fire"]}])
   });
 
+  it('debería retornar "Bulbasaur para cuando filtro por tipo Grass"', () => {
+    assert.deepEqual(window.filterData(data,'Grass'), [{
+      "num": "001","name": "Bulbasaur", "type": ["Grass", "Poison"]}])
+  });
+
+
+  it('debería retornar "Bulbasaur para cuando filtro por tipo Poison"', () => {
+    assert.deepEqual(window.filterData(data,'Poison'), [{
+      "num": "001","name": "Bulbasaur", "type": ["Grass", "Poison"]}])
+  });
+
+
+  it('debería retornar "Caterpie para cuando filtro por tipo Bug"', () => {
+    assert.deepEqual(window.filterData(data,'Bug'), [{
+      "num": "010","name": "Caterpie", "type": ["Bug"]}])
+  });
 
 
 });
 
 describe('sortData', () => {
+
   
   it('debería ser una función', () => {
     assert.equal(typeof sortData, 'function');
   });
+
+
+  it('debería retornar la data ordenada a-z por nombre', () => {
+    assert.deepEqual(window.filterData(data,"num","ascaz"), [
+       {"num": "001",
+      "name": "Bulbasaur",
+      "type": [
+        "Grass",
+        "Poison"]
+      },  
+      
+      {"num": "010",
+      "name": "Caterpie",
+      "type": [
+        "Bug"]
+      },
+             
+        {"num": "004",
+      "name": "Charmander",
+      "type": [
+        "Fire"]
+      } 
+  ]
+  )
+  });
+
+
 
 });
