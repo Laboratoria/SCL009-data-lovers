f2bf832e0e8059dc190b0ec1980fecf38acd5577
Cloiw <sircli@hotmail.com>
2019-05-06 20:28:46 -0400

    info,css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /* Manejo del DOM */
-const championInfoDiv = document.getElementById("championInfo");
+
 const championImgDiv = document.getElementById("championImg");
 const orderChange = document.getElementById("userSelectedOrder");
 const selectChange = document.getElementById("userSelectedTag");
@@ -9,13 +9,32 @@ let champName;
 let champDifficulty;
 
 
- const championInfo = (InfoHp,InfoHpLvl) =>{
+ const championInfo = (infoName,infoHp,infoHpLvl,infoMp,infoMpLvl,infoAttack,infoMS,infoArmor,infoArmorLvl,infoSpellBlock,infoSpellBlockLvl,infoAttackRange,
+  infoHpRegen,infoHpRegenLvl) =>{
       const championInfoDiv = document.getElementById("championInfo");
       championInfoDiv.innerHTML=""
        championInfoDiv.innerHTML += 
        
+       
        `<div>
-         <p> HP : ${InfoHp} (+${InfoHpLvl} por nivel) <br>
+         <p align="center" class="infoChampClass"> 
+         ${infoName}<br><br>
+         <strong>Vida</strong><br>
+         ${infoHp} (+${infoHpLvl} por nivel) <br> 
+             <strong>Mana</strong> <br>
+             ${infoMp} (+${infoMpLvl} por nivel) <br>
+             <strong>Ataque</strong><br>
+             ${infoAttack} <br>
+             <strong>Velocidad Movimiento</strong><br>
+              ${infoMS} <br>
+             <strong>Armadura</strong><br>
+             ${infoArmor} (+${infoArmorLvl})<br>
+             <strong>Resistencia mágica</strong><br>
+             ${infoSpellBlock} (+${infoSpellBlockLvl})<br>
+             <strong>Rango de ataque</strong><br>
+             ${infoAttackRange}<br>
+             <strong>Regeneración de vida</strong><br>
+              ${infoHpRegen} (+${infoHpRegenLvl})<br>
          </p>
        </div>`;
        
@@ -24,43 +43,31 @@ window.onload = function showAllOnload() {  //funcion para mostrar todos al inic
  
   let allResultFilter = window.filteringResult("Todos",dataLol)
      allResultFilter.forEach(element => {
-    champName= element.name;
-    champImg= element.img;
-    champInfoHp= element.stats.hp;
-    champInfoHpLvl= element.hpperlevel;
-    console.log(champInfoHp)
-
-
-
-
-   championImgDiv.innerHTML += 
+     console.log(element.title);
+    championImgDiv.innerHTML += 
+    
    `<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
     <div class="card mt-4" style="background-color: #e2e2e2";>
-        <button id="${champName}">
+    <div style="cursor: pointer;" id="${element.name}" onclick="championInfo(${element.tittle},${element.stats.hp},${element.stats.hpperlevel},${element.stats.mp},${element.stats.mpperlevel},${element.stats.attackdamage},${element.stats.movespeed},
+      ${element.stats.armor},${element.stats.armorperlevel},${element.stats.spellblock},${element.stats.spellblockperlevel},${element.stats.attackrange},${element.stats.hpregen},${element.stats.hpregenperlevel})">
         <div class="card-header">
-        <img src="${champImg}">
+        <img src="${element.img}">
         <div class="card-content">
-        <p class="pname">${champName}<p></button>
+        <p class="pname">${element.name}<p></div>
         
            </div>
        </div> 
     </div>
   </div>`    
   
-  document.getElementById(champName).onclick = championInfo(champName,champInfoHp);
+  
  
  
      });
       
 
   }
-  let buttons = document.getElementsByTagName('button');
   
-
-  for (let e = 0; e<buttons.length; ++e) {
-    Allbuttons = buttons[e];
-  }
-
   
 
     
