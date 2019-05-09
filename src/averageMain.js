3b8f922f1f4a9d7a8821608d3a5cede0a9b2afe4
rositadiazgalaz <rositadiazgalaz@gmail.com>
2019-05-09 11:36:06 -0400

    readme listo

--- src/averageMain.js
+++ src/averageMain.js
@@ -1,20 +1,7 @@
 //addevent de boton calcular
 //console.log(window.average("Jinx","Bard","Aatrox","Nami", "Soraka", dataLol))
 
-<<<<<<< HEAD
-const lolData = window.LOLdata;
-let calculateBtn= document.getElementById("btnCalculateAverage");
-calculateBtn.addEventListener("click", ()=>{
-  let showMyTeamAverage= document.getElementById("average");
-  let topAverage =document.getElementById("topFirstTeam").value;
-  let jungleAverage=document.getElementById("jungleFirstTeam").value;
-  let midAverage=document.getElementById("midFirstTeam").value;
-  let adcAverage=document.getElementById("adcFirstTeam").value;
-  let supportAverage=document.getElementById("supportFirstTeam").value;
- 
-  let calculateAverage = window.average(topAverage,jungleAverage , midAverage, adcAverage, 
-   supportAverage, lolData);
-=======
+
 
 
 let calculateBtn= document.getElementById("btnCalculateAverage");
@@ -25,7 +12,7 @@ calculateBtn.addEventListener("click", ()=>{
   let midTotal=document.getElementById("midFirstTeam").value;
   let adcTotal=document.getElementById("adcFirstTeam").value;
   let supportTotal=document.getElementById("supportFirstTeam").value;
->>>>>>> ba971e69fdebd9afb67bfa31713456cbdfe04b26
+
   
   
   let calculateAllAttacks= window.calculateAllAttacks(topTotal,jungleTotal,midTotal,adcTotal,supportTotal,window.LOLdata)
